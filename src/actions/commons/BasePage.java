87a107ceef9e44f909889aba7712b31d9b888895
ThuyPham <thuypb88@gmail.com>
2021-10-07 07:38:07 +0700

    Framework 03 - Build BasePage class

--- src/actions/commons/BasePage.java
+++ src/actions/commons/BasePage.java
@@ -1,4 +1,133 @@
 package actions.commons;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+
+// Common class
 public class BasePage {
+    WebDriver driver;
+
+    // Nhiệm vụ mở 1 Url ra
+    // Common function
+    public void openPageUrl(WebDriver driver, String pageUrl){
+        driver.get(pageUrl);
+    }
+
+    public String getPageTitle(WebDriver driver){
+        return driver.getTitle();
+    }
+
+    public String getPageUrl(WebDriver driver){
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageSourceCode(WebDriver driver){
+        return driver.getPageSource();
+    }
+    public void backToPage(WebDriver driver){
+        driver.navigate().back();
+    }
+
+    public void forwardToPage(WebDriver driver){
+        driver.navigate().forward();
+    }
+
+    public void refreshCurrentPage(WebDriver driver){
+        driver.navigate().refresh();
+    }
+
+/*    public void waitForAlertPresence(WebDriver driver){
+        WebDriverWait explicitWait = new WebDriverWait(driver, 30);
+        explicitWait.until(ExpectedConditions.alertIsPresent());
+    }*/
+    public Alert waitForAlertPresence(WebDriver driver){
+        WebDriverWait explicitWait = new WebDriverWait(driver, 30);
+        return explicitWait.until(ExpectedConditions.alertIsPresent());
+    }
+
+/*    public void acceptAlert(WebDriver driver){
+        driver.switchTo().alert().accept();
+       }
+
+       public void acceptAlert(WebDriver driver){
+            Alert alert = waitForAlertPresence(driver);
+            alert.accept();
+        }
+    }*/
+    public void acceptAlert(WebDriver driver){
+        waitForAlertPresence(driver).accept();
+    }
+
+    public void cancelAlert(WebDriver driver){
+        waitForAlertPresence(driver).dismiss();
+    }
+
+    public String getAlertText(WebDriver driver){
+        return waitForAlertPresence(driver).getText();
+    }
+
+    public void sendkeyToAlert(WebDriver driver, String textValue){
+        waitForAlertPresence(driver).sendKeys(textValue);
+    }
+
+    public void switchToWindowByID(WebDriver driver, String windowID){
+        Set<String> allWindowIDs = driver.getWindowHandles();
+        for(String id : allWindowIDs){
+            if(!id.equals(windowID)){
+                driver.switchTo().window(id);
+                break;
+            }
+        }
+    }
+
+    public void switchToWindowByTitle(WebDriver driver, String tabTitle){
+        Set<String> allWindowIDs = driver.getWindowHandles();
+        for(String id : allWindowIDs){
+            driver.switchTo().window(id);
+            String actualTitle = driver.getTitle();
+            if(actualTitle.equals(tabTitle)){
+               break;
+            }
+        }
+    }
+
+    public void closeAllTabWithoutParent(WebDriver driver, String parentID){
+        Set<String> allWindowIDs = driver.getWindowHandles();
+        for(String id : allWindowIDs){
+            if(!id.equals(parentID)){
+                driver.switchTo().window(id);
+                driver.close();
+            }
+            driver.switchTo().window(parentID);
+        }
+    }
+
+    public WebElement getWebElement(WebDriver driver, String xpathLocator){
+        return driver.findElement(By.xpath(xpathLocator));
+    }
+
+    public void clickToElement(WebDriver driver, String xpathLocator){
+        getWebElement(driver, xpathLocator).click();
+    }
+
+/*    public void sendkeyToElement(WebDriver driver, String xpathLocator, String textValue){
+        getWebElement(driver, xpathLocator).clear();
+        getWebElement(driver, xpathLocator).sendKeys(textValue);
+    }*/
+
+    public void sendkeyToElement(WebDriver driver, String xpathLocator, String textValue){
+        WebElement element =  getWebElement(driver, xpathLocator);
+        element.clear();
+        element.sendKeys(textValue);
+    }
+
+    public void getElementText(WebDriver driver, String xpathLocator){
+        getWebElement(driver, xpathLocator).getText();
+    }
 }
---
+++ src/testcases/com/nopcommerce/user/Level_01_Apply_BasePage_1_Login.java
@@ -0,0 +1,46 @@
+package testcases.com.nopcommerce.user;
+
+import actions.commons.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Level_01_Apply_BasePage_1_Login {
+    WebDriver driver;
+    String emailAddress;
+    // Declare (khai báo)
+    BasePage basePage;
+    // BasePage : class
+    // basePage: Object
+
+    String projectPath = System.getProperty("user.dir");
+
+    @BeforeClass // Multiple browsers
+    public void beforeClass(){
+        System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        // Driver có ID rồi
+
+        // Initial (Khởi tạo)
+        basePage = new BasePage();
+
+        emailAddress = "afc" + "@mailinator.com";
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://demo.nopcommerce.com/");
+    }
+
+    @Test
+    public void f(){
+    }
+    @BeforeClass
+    public void beforeClass(){
+    }
+    @AfterClass // Custom
+    public void afterClass(){
+    }
+
+}
